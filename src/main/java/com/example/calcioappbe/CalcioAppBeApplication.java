20f9d2a2e1d3eca9bc228098f9d70500bf4d1e5c
Nicola Dipinto <n.dipinto197@gmail.com>
2025-03-31 00:54:19 +0200

    first commit

---
+++ src/main/java/com/example/calcioappbe/CalcioAppBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.calcioappbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcioAppBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalcioAppBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/calcioappbe/CalcioAppBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.calcioappbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcioAppBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
